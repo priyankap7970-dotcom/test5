620a5b618c657e4922560587417ee1bfd627039a
priyanka <priyankap7970@gmail.com>
2026-01-12 17:50:49 +0530

    Update Test5Application.java

--- test5/src/main/java/com/test5/Test5Application.java
+++ test5/src/main/java/com/test5/Test5Application.java
@@ -7,7 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test5Application {
 
 	public static void main(String[] args) {
+		String name = "mike";
 		int x=100;
+		boolean isPresent=false;
+		int y=200;
+		char x='a';
+		int y=200;
+		double z=10.3f;
+		boolean isPresent=false;
+		float m=10.3f;
+		char ch='x';
 		SpringApplication.run(Test5Application.class, args);
 	}
 
